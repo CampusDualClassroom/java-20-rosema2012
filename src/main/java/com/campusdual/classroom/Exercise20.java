bdb2a34ce003e9487dc714bc2db7ceb5ce0b8f44
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-27 09:35:20 +0200

    Ejercicio 20 terminado

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -1,16 +1,30 @@
 package com.campusdual.classroom;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise20 {
 
-    public static List<Person> getPeopleList() {
 
+    public static List<Person> getPeopleList() {
+        List<Person> l = new ArrayList<>();
+        Person p1 = new Person("John", "Smith");
+        Person p2 = new Teacher("Maria", "Montesosori", "Educación");
+        Person p3 = new PoliceOfficer("Jake","Peralta","B-99");
+        Person p4 = new Doctor("Gregory", "House", "Nefrología e infectología");
+        l.add(p1);
+        l.add(p2);
+        l.add(p3);
+        l.add(p4);
+        return l;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
+        for (int i = 0; i < stringList.size(); i++) {
+            stringList.get(i).getDetails();
 
+        }
     }
 
     public static void main(String[] args) {
